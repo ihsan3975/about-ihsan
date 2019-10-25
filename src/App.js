2d2aa6536d07f7e39ad41e922703877cef9cc113
ihsan3975 <m.ihsan3975@gmail.com>
2019-10-25 09:02:10 +0700

    new test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hi, my name Maulana Ihsan
         </a>
       </header>
     </div>
