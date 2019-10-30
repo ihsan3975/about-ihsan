a27fb19a760bb106534be3fa76645a1b55e9ae61
ihsan3975 <m.ihsan3975@gmail.com>
2019-10-30 14:21:42 +0700

    bug fixed

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'react-app-polyfill/stable';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
